e32bcce2e8a09c1b99ef14592c25f96469791a80
drake wilcox and jeremy kale padot <jeremy@>
2020-01-29 14:00:06 -0800

    add functions that seperate number generator and if statements. working as player 1 turn score

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 // Business Logic For Dice Roll
 
 function Player1() {
-  this.roll = 0
+  this.turnScore = 0;
+}
+
+generateNumber = function() {
+  var numberVal = Math.floor( Math.random() *6) + 1;
+  return numberVal;
 }
 
 Player1.prototype.diceRoll = function() {
-  var numberVal =  Math.floor( Math.random() *6) + 1;
-  if (numberVal === 1){
-    return numberVal * 0
+  var roll = generateNumber();
+  if (roll !=1)  {
+    return this.turnScore += roll;
+  } else if (roll = 1)  {
+    return this.turnScore *= 0;
+    }
   }
-   return this.roll + numberVal
-}
-
-  
 
 
 
@@ -20,7 +24,6 @@ Player1.prototype.diceRoll = function() {
 var turn = new Player1();
 $(document).ready(function() {
   $(".rollBtn").click(function(){
-
 
    var result = turn.diceRoll();
    $("#p1turn-total").text(result);
