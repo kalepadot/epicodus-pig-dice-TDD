5735e003dda2bf3a0c90e2917558293d8d53de0d
drake wilcox and jeremy kale padot <jeremy@>
2020-01-29 13:37:08 -0800

    add a functional dice roll, when roll 1, numberVal is * by 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 // Business Logic For Dice Roll
 
-// DiceValue.prototype.generateNumber = function(){
-//   return Math.floor(Math.random() * 6) + 1;
 function Player1() {
   this.roll = 0
 }
 
 Player1.prototype.diceRoll = function() {
   var numberVal =  Math.floor( Math.random() *6) + 1;
-  return this.roll + numberVal
+  if (numberVal === 1){
+    return numberVal * 0
+  }
+   return this.roll + numberVal
 }
 
   
@@ -19,7 +20,7 @@ Player1.prototype.diceRoll = function() {
 var turn = new Player1();
 $(document).ready(function() {
   $(".rollBtn").click(function(){
-    // var player1score = diceRoll();
+
 
    var result = turn.diceRoll();
    $("#p1turn-total").text(result);
