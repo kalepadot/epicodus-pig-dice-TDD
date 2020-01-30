f830372923021dea8ee23ce5ad12e91ec303ab73
Student <example@example.com>
2020-01-30 11:04:37 -0800

    add funcitons that allow for two players and seperate results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 // Business Logic For Dice Roll
 
-function Player1() {
+function Player() {
   this.turnScore = 0;
   this.scoreTotal = 0;
+  this.scoreTotal2 = 0;
 }
+
 
 
 generateNumber = function() {
@@ -11,7 +13,7 @@ generateNumber = function() {
   return numberVal;
 }
 
-Player1.prototype.diceRoll = function() {
+Player.prototype.diceRoll = function() {
   var roll = generateNumber();
   if (roll !=1)  {
     return this.turnScore += roll;
@@ -19,18 +21,31 @@ Player1.prototype.diceRoll = function() {
     return this.turnScore *= 0;
     }
   }
-  Player1.prototype.holdTransfer = function() {
+  Player.prototype.holdTransfer = function() {
    var holdNumber = this.turnScore;
    return this.scoreTotal += holdNumber;
   }
  
-Player1.prototype.clearTurnScore = function() {
+Player.prototype.clearTurnScore = function() {
   this.turnScore *=0;
   return this.turnScore;
 }
+// player2
+Player.prototype.holdTransfer2 = function() {
+  var holdNumber = this.turnScore;
+  return this.scoreTotal2 += holdNumber;
+ }
+
+Player.prototype.clearTurnScore2 = function() {
+ this.turnScore *=0;
+ return this.turnScore;
+}
 // User Logic
-var turn = new Player1();
+var turn = new Player;
+ 
+
 $(document).ready(function() {
+  // player 1 
   $(".rollBtn").click(function(){
    var result = turn.diceRoll();
    $("#p1turn-total").text(result);
@@ -38,10 +53,21 @@ $(document).ready(function() {
   $(".hold").click(function()  {
     var result2 = turn.holdTransfer();
     var result3 = turn.clearTurnScore();
-    
-    
+
+
     $("#p1score-total").text(result2);
 
     $("#p1turn-total").text(result3);
+  });
+  // player2
+  $(".rollBtn2").click(function() {
+    var result = turn.diceRoll();
+    $("#p2turn-total").text(result);
+  });
+  $(".hold2").click(function(){
+    var result2 = turn.holdTransfer2();
+    var result3 = turn.clearTurnScore2();
+    $("#p2score-total").text(result2);
+    $("#p2turn-total").text(result3);
   });
 });
